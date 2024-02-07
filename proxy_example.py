c5faadd432843cc7b006159b54446a6d2cecc958
Peter Sulcs <Peter.Sulcs@gmail.com>
2024-02-06 21:33:10 -0800

    maybe?

--- proxy_example.py
+++ proxy_example.py
@@ -1,29 +1,50 @@
-from flask import Flask, request
+import requests
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import requests
 
-app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'DELETE'])
-@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def proxy(path):
-    # Get the request data
-    request_data = request.get_data()
+class ProxyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.proxy_request()
 
-    # Modify the request body as needed
-    modified_request_data = modify_request_body(request_data)
+    def do_POST(self):
+        self.proxy_request()
 
-    # Make the request to the target server
-    target_url = 'http://target-server.com/' + path
-    response = requests.request(request.method, target_url, data=modified_request_data, headers=request.headers)
+    def do_PUT(self):
+        self.proxy_request()
 
-    # Return the response from the target server
-    return response.content, response.status_code, response.headers.items()
+    def do_DELETE(self):
+        self.proxy_request()
+
+    def proxy_request(self):
+        # Get the request data
+        content_length = int(self.headers['Content-Length'])
+        request_data = self.rfile.read(content_length)
+
+        # Modify the request body as needed
+        modified_request_data = modify_request_body(request_data)
+
+        # Make the request to the target server
+        target_url = 'http://target-server.com' + self.path
+        response = requests.request(self.command, target_url, data=modified_request_data, headers=self.headers)
+
+        # Send the response from the target server back to the client
+        self.send_response(response.status_code)
+        for header, value in response.headers.items():
+            self.send_header(header, value)
+        self.end_headers()
+        self.wfile.write(response.content)
+
+def run_proxy_server():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, ProxyHandler)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run_proxy_server()
 
 def modify_request_body(request_data):
     # Modify the request body here
     modified_request_data = request_data.upper()
 
     return modified_request_data
-
-if __name__ == '__main__':
-    app.run()
